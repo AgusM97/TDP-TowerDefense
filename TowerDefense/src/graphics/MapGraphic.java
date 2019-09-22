f2df9604772dcafa6709f057e326a80b7c43343b
IVChristos <anonymousvsworld@users.noreply.github.com>
2019-09-22 13:46:45 -0300

    ultimo de isometrico
    faltaria agrandarlo y ademas ponerle al frame el jscroll pane y estariamos, pero no tengo idea de como hacerlo

--- TowerDefense/src/graphics/MapGraphic.java
+++ TowerDefense/src/graphics/MapGraphic.java
@@ -4,6 +4,7 @@ import java.awt.Image;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
 
 public class MapGraphic {
 	protected JLabel grafico;
@@ -11,9 +12,12 @@ public class MapGraphic {
 	protected final int height = 900;
 	
 	public MapGraphic() {
-		grafico = new JLabel(new ImageIcon(this.getClass().getResource("/img/asd2.png")));
+		grafico = new JLabel(new ImageIcon(this.getClass().getResource("/img/coso.jpg")));
 		grafico.setVisible(true);
 		grafico.setBounds(0, 0, width, height);
+		
+		
+		
 	}
 	
 	public JLabel getGrafico() {
