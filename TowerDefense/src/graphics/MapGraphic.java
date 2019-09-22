047a4ee2a94cf2a3bc19bf149fa2cc82c2658430
IVChristos <anonymousvsworld@users.noreply.github.com>
2019-09-22 10:15:32 -0300

    cambio en mapgraphic

--- TowerDefense/src/graphics/MapGraphic.java
+++ TowerDefense/src/graphics/MapGraphic.java
@@ -7,15 +7,11 @@ import javax.swing.JLabel;
 
 public class MapGraphic {
 	protected JLabel grafico;
-	protected final int width = 1280;
-	protected final int height = 720;
+	protected final int width = 1440;
+	protected final int height = 900;
 	
 	public MapGraphic() {
-		grafico = new JLabel(new ImageIcon
-				(new ImageIcon(this.getClass().getResource("/img/9x9undertile.png")).getImage().getScaledInstance(200,200, Image.SCALE_SMOOTH))
-				);
-				
-				new JLabel(new ImageIcon(this.getClass().getResource("/img/9x9undertile.png")));
+		grafico = new JLabel(new ImageIcon(this.getClass().getResource("/img/asd2.png")));
 		grafico.setVisible(true);
 		grafico.setBounds(0, 0, width, height);
 	}
