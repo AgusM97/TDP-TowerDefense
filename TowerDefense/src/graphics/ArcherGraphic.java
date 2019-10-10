9a11e2aec115fe4582dc97be494554e1aaab473e
IVChristos <anonymousvsworld@users.noreply.github.com>
2019-10-10 17:03:57 -0300

    Añadido graphics y gif
    El Idle de Archer

---
+++ TowerDefense/src/graphics/ArcherGraphic.java
@@ -0,0 +1,11 @@
+package graphics;
+
+import java.awt.Rectangle;
+
+public class ArcherGraphic extends TowerGraphic{
+	
+	public ArcherGraphic(Rectangle hb) {
+		super(hb, "/gif/Archer-Idle.gif");
+	}
+
+}
