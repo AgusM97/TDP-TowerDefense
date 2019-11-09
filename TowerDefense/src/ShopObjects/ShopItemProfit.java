51917606d9e1e46cd0c35d1fee9c2d1a5f150d08
Agustin Montero <agus91997@gmail.com>
2019-11-08 23:24:43 -0300

    profit fix

--- TowerDefense/src/ShopObjects/ShopItemProfit.java
+++ TowerDefense/src/ShopObjects/ShopItemProfit.java
@@ -1,13 +1,15 @@
 package ShopObjects;
 
+import java.util.Random;
+
 import Game.Game;
 
 public class ShopItemProfit{
 
 	public static boolean profitOrNot() {
-		double x = Math.random();
+		Random r = new Random();
 		Game juego = Game.getInstance();
-		if((x%2)==0) {
+		if(r.nextInt(2)==0) {
 			juego.addCoins(100);
 			return true;
 		}
